65a1b7beeb4990b9d24a0d3015d2db975b73ac66
Katherine Rodriguez <davjdadlfls@gmail.com>
2019-12-27 01:20:06 -0500

    11 commit

--- app/Administrator.php
+++ app/Administrator.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Administrator extends Model
 {
+
+    public function validationRules(){
+        return [
+                'id'      =>  'required', 
+                'peoples_id'       =>  'required',
+                'username'         =>  'required',
+                'password'         =>  'required',
+        ];
+    }
+
     public function People(){
         return $this->belongsTo('App\People');
     }
