b536e6062c19fb2cf63679cef98c275e220c123a
Katherine Rodriguez <davjdadlfls@gmail.com>
2019-12-27 00:16:01 -0500

    quinto commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AdministratorSeeder::class);
+        $this->call(peopleSeeder::class);
     }
 }
